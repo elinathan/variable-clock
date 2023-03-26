b43bbf587592c466b7064d3abd7274ebba87b62e
eli <enathan@seas.upenn.edu>
2023-03-26 19:52:07 -0400

    simplify and comment code

--- sketch.js
+++ sketch.js
@@ -1,30 +1,36 @@
-import './style.css'
+import './style.css';
 import p5 from 'p5';
 
-// p5
+// define a p5 sketch function
 const p5Function = (p) => {
+  // declare variables
   let time;
-  let img;
   let dateTime;
   const bg = "#EDD6C3";
+
+  // set the initial title of the page to the current time
   document.title = `${p.hour().toString().padStart(2, '0')}:${p.minute().toString().padStart(2, '0')}`;
 
+  // set up the p5 canvas
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
-    p.background(bg); //D74532
+    p.background(bg);
+    // create a <p> element to display the time
     time = p.createP(timeString(false));
-    time.style('color', '#0032FF'); //#F19B38
+    time.style('color', '#0032FF');
+    // create an <h1> element to display the full date and time
     dateTime = p.createElement('h1', displayTime(timeString(true)));
+    // set the frame rate to 4 frames per second
     p.frameRate(4);
-    console.log(p.select("p"));
-    console.log(p.windowWidth);
   }
 
+  // draw loop
   p.draw = () => {
-    p.background(bg); //D74532
-    // p.image(img, 0, 0);
-    time.html(timeString(false)); //:${pad(p.second(), 2)}
+    p.background(bg);
+    // update the time and date display
+    time.html(timeString(false));
     dateTime.html(displayTime(timeString(true)));
+    // adjust the font width based on the canvas dimensions
     const fontWidth = 350 * p.windowWidth / p.windowHeight;
     time.style('font-variation-settings', `'wdth' ${fontWidth}`);
   }
@@ -34,6 +40,7 @@ const p5Function = (p) => {
     p.resizeCanvas(p.windowWidth, p.windowHeight);
   }
 
+  // function to generate the time string with a blinking colon
   function timeString(isSmall) {
     return `${p.hour().toString().padStart(2, '0')}<span style='font-feature-settings: "aalt"; opacity: ${colonBlink(isSmall)};'>:</span>${p.minute().toString().padStart(2, '0')}`
   }
@@ -51,9 +58,10 @@ const p5Function = (p) => {
   }
 }
 
-// create a new instance of p5 and pass in the function
+// create a new instance of p5 and pass in the sketch function
 new p5(p5Function);
 
+// function to generate the full date and time string
 function displayTime(time) {
   const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const monthsOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -63,22 +71,30 @@ function displayTime(time) {
   const dayOfMonth = now.getDate().toString();
   const ordinal = getOrdinalSuffix(dayOfMonth);
   const year = now.getFullYear();
+  // return the full date and time string
   return `The time is ${time} on ${dayOfWeek} <span id="no-wrap">${monthOfYear} ${dayOfMonth}<span id="ordinal">${ordinal}</span></span>, ${year}`;
 }
 
+// function to get the ordinal suffix for a day of the month
 function getOrdinalSuffix(day) {
+  // array of suffixes for day of the month
   const suffixes = ['th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th'];
+  // special case for days 11-13
   if (day >= 11 && day <= 13) {
     return 'th';
   }
+  // return the appropriate suffix based on the last digit of the day
   return suffixes[day % 10];
 }
 
+// function to update the page title with the current time
 function updateTimeInTitle() {
   const now = new Date();
   const hours = now.getHours().toString().padStart(2, '0');
   const minutes = now.getMinutes().toString().padStart(2, '0');
+  // set the title to the current time in the format "HH:MM"
   document.title = `${hours}:${minutes}`;
 }
 
-setInterval(updateTimeInTitle, 1000); // call the function every second to update the time
+// call the function to update the page title every second
+setInterval(updateTimeInTitle, 1000);
